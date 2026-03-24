4c4465c6e186d82926f0deb1133fa68c2efb46dc
Mano <mano.raichon@orange.fr>
2026-03-24 12:07:23 +0100

    Fix header vertical align

--- components/header/AppHeader.tsx
+++ components/header/AppHeader.tsx
@@ -4,8 +4,8 @@ import { HeaderActions } from "@/components/header/HeaderActions";
 export function AppHeader() {
   return (
     <header className="border-b-4 border-[#2d7bf4]">
-      <div className="w-full bg-[#0F1941] px-6 pt-6 text-white">
-        <div className="flex items-center justify-between gap-4 pb-4">
+      <div className="w-full bg-[#0F1941] px-6 text-white">
+        <div className="flex min-h-[76px] items-center justify-between gap-4">
           <HeaderBrand />
           <HeaderActions />
         </div>
